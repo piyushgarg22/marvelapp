093f230326cfece4f71fe5b443da0f9d7f51481e
piyushgarg22 <pgarg2020@gmail.com>
2020-06-01 21:47:33 +0530

    Updates

---
+++ precache-manifest.53d1a80abcd657d8b18df6b1a00882dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d722c2c227c8dcc0e6f7de5fac99f2e9",
+    "url": "/marvelapp/index.html"
+  },
+  {
+    "revision": "c80a967dab647ce62d37",
+    "url": "/marvelapp/static/css/main.f2086846.chunk.css"
+  },
+  {
+    "revision": "c436e7b80a75c9656bc3",
+    "url": "/marvelapp/static/js/2.4c6a13fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marvelapp/static/js/2.4c6a13fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80a967dab647ce62d37",
+    "url": "/marvelapp/static/js/main.989bc4b7.chunk.js"
+  },
+  {
+    "revision": "ced0b5a8ed32bc333ced",
+    "url": "/marvelapp/static/js/runtime-main.62959b28.js"
+  }
+]);
